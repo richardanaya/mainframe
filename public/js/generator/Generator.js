eaaf8fe6dc15b4eb1bdc25968e8d5f5b1b4c86d7
Howard N Smith <howard@blacktriangles.com>
2014-03-01 06:35:05 -0600

    Rooms pt1

--- public/js/generator/Generator.js
+++ public/js/generator/Generator.js
@@ -7,11 +7,7 @@ Generator.prototype.generateLevel = function(width,height,tileset) {
 	result.width = width;
 	result.height = height;
 
-	for( var y = 0; y < height; y++ ) {
-		for( var x = 0; x < width; x++ ) {
-			result.tiles[y*result.width+x] = { type: Level.Types.Floor, image: result.tileset.getRandomFloor(), objects:[] };
-		}
-	}
+	this.generateLayout( result );
 
 	return result;
 }
@@ -21,10 +17,11 @@ Generator.prototype.generateLayout = function( level ) {
 }
 
 Generator.prototype.addRoom = function( level ) {
-	var width = Utilities.randRangeInt( 5, 10 );
-	var height = Utilities.randRangeInt( 5, 10 );
+	var width = 5;
+	var height = 5;
 	for( var y = 0; y < height; y++ ) {
 		for( var x = 0; x < width; x++ ) {
+			level.tiles[y*level.width+x] = { type: Level.Types.Floor, image: level.tileset.getRandomFloor(), objects:[] };
 		}
 	}
 }
