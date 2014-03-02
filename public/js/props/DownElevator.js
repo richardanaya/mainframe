8c6d43fc9df4ecca75c6d46bd59153b3f30da7e4
Richard <richard.anaya@gmail.com>
2014-03-01 18:43:08 -0600

    fix for elevator

--- public/js/props/DownElevator.js
+++ public/js/props/DownElevator.js
@@ -2,12 +2,13 @@ var DownElevator = function(){
     this.x = 0;
     this.y = 0;
     this.image = Resources.images.elevator;
-    this.tags = ["elevator"];
+    this.tags = ["solid","elevator"];
 }
 
 
 DownElevator.prototype = Object.create(GameObject.prototype);
 
 DownElevator.prototype.onObjectEnter = function(o){
+    if(o.tags.indexOf('player') == -1){ return; }
     this.level.scene.loadLevel(this.level.scene.currentHeight-100);
 }
--- public/js/props/UpElevator.js
+++ public/js/props/UpElevator.js
@@ -2,12 +2,13 @@ var UpElevator = function(){
     this.x = 0;
     this.y = 0;
     this.image = Resources.images.up_elevator;
-    this.tags = ["elevator"];
+    this.tags = ["solid","elevator"];
 }
 
 
 UpElevator.prototype = Object.create(GameObject.prototype);
 
 UpElevator.prototype.onObjectEnter = function(o){
+    if(o.tags.indexOf('player') == -1){ return; }
     this.level.scene.loadLevel(this.level.scene.currentHeight+100);
 }
