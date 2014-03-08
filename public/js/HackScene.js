313793379a0bb7efaf086dd80bf22c43e31ebd2f
JackStandard <jackreed4@gmail.com>
2014-03-07 23:48:33 -0600

    hack scene improvements

--- public/js/HackScene.js
+++ public/js/HackScene.js
@@ -8,6 +8,12 @@ var HackScene = function(game, returnScene, playerImage){
     this.backgroundColor = "#9900FF";
     this.phasingIn = true;
     this.phaseInSpeed = 1.5;
+    this.minimumSquareSize = 75;
+    this.squareSize = 70;
+    this.upLeftGridCornerX = 0;
+    this.upLeftGridCornerY = 0;
+    this.gridObjectSize = 0;
+    this.gridObjectPadding = 5;
 };
 
 HackScene.prototype = Object.create(Scene.prototype);
@@ -17,6 +23,28 @@ HackScene.prototype.update = function(delta){
     
     this.ctx.fillStyle = "black";
     this.ctx.fillRect(0,0,this.width,this.height);
+
+    /*
+    this.ctx.font = "12px 'Press Start 2P'";
+    this.ctx.fillStyle = "white";
+    this.ctx.globalAlpha = 1;
+    this.ctx.fillText((this.height/10).toString(), 10, 100);
+    */
+
+    
+    if (this.width/20 < this.minimumSquareSize || this.height/10 < this.minimumSquareSize )
+    {
+        this.squareSize = this.minimumSquareSize;
+    }
+    else
+    {
+        this.squareSize = this.width/20;
+    }
+
+    this.gridObjectSize = this.squareSize - (this.gridObjectPadding * 2);
+
+    this.upLeftGridCornerX = this.width/4;
+    this.upLeftGridCornerY = this.height * .1;
     
     if (this.time >= this.phaseInSpeed)
     {
@@ -30,8 +58,8 @@ HackScene.prototype.update = function(delta){
         for (i=0; i<11;i++)
         {
             this.ctx.beginPath();
-            this.ctx.moveTo(i * (this.width/20) + this.width/4, 0);
-            this.ctx.lineTo(i * (this.width/20) + this.width/4, 0 + (this.time/this.phaseInSpeed) * (this.height/2));
+            this.ctx.moveTo(i * this.squareSize + this.upLeftGridCornerX, 0);
+            this.ctx.lineTo(i * this.squareSize + this.upLeftGridCornerX, 0 + (this.time/this.phaseInSpeed) * (this.height/2));
             this.ctx.stroke();
         }
 
@@ -39,8 +67,8 @@ HackScene.prototype.update = function(delta){
         for (i=0; i<11;i++)
         {
             this.ctx.beginPath();
-            this.ctx.moveTo(i * (this.width/20) + this.width/4, this.height);
-            this.ctx.lineTo(i * (this.width/20) + this.width/4, this.height - ((this.time/this.phaseInSpeed) * (this.height/2)));
+            this.ctx.moveTo(i * this.squareSize + this.upLeftGridCornerX, this.height);
+            this.ctx.lineTo(i * this.squareSize + this.upLeftGridCornerX, this.height - ((this.time/this.phaseInSpeed) * (this.height/2)));
             this.ctx.stroke();
         }
         
@@ -48,16 +76,16 @@ HackScene.prototype.update = function(delta){
         for (i=0; i<9;i++)
         {
             this.ctx.beginPath();
-            this.ctx.moveTo(0, i * (this.height/10) + this.height * .1);
-            this.ctx.lineTo(0 + (this.time/this.phaseInSpeed) * (this.width/2), i * (this.height/10) + this.height * .1);
+            this.ctx.moveTo(0, i * this.squareSize + this.upLeftGridCornerY);
+            this.ctx.lineTo(0 + (this.time/this.phaseInSpeed) * (this.width/2), i * this.squareSize + this.height * .1);
             this.ctx.stroke();
         }
 
         for (i=0; i<9;i++)
         {
             this.ctx.beginPath();
-            this.ctx.moveTo(this.width, i * (this.height/10) + this.height * .1);
-            this.ctx.lineTo(this.width - ((this.time/this.phaseInSpeed) * (this.width/2)), i * (this.height/10) + this.height * .1);
+            this.ctx.moveTo(this.width, i * this.squareSize + this.upLeftGridCornerY);
+            this.ctx.lineTo(this.width - ((this.time/this.phaseInSpeed) * (this.width/2)), i * this.squareSize + this.height * .1);
             this.ctx.stroke();
         }
         
@@ -78,20 +106,22 @@ HackScene.prototype.update = function(delta){
         for (i=0; i<11;i++)
         {
             this.ctx.beginPath();
-            this.ctx.moveTo(i * (this.width/20) + this.width/4, this.height * .1);
-            this.ctx.lineTo(i * (this.width/20) + this.width/4, this.height * .9);
+            this.ctx.moveTo(i * this.squareSize + this.upLeftGridCornerX, this.upLeftGridCornerY);
+            this.ctx.lineTo(i * this.squareSize + this.upLeftGridCornerX, this.upLeftGridCornerY + (this.squareSize * 8));
             this.ctx.stroke();
         }
     
         for (i=0; i<9;i++)
         {
             this.ctx.beginPath();
-            this.ctx.moveTo(this.width/4, i * (this.height/10) + this.height * .1);
-            this.ctx.lineTo(10 * (this.width/20) + this.width/4, i * (this.height/10) + this.height * .1);
+            this.ctx.moveTo(this.upLeftGridCornerX, i * this.squareSize + this.upLeftGridCornerY);
+            this.ctx.lineTo(10 * this.squareSize + this.upLeftGridCornerX, i * this.squareSize + this.upLeftGridCornerY);
             this.ctx.stroke();
         }
 
-        this.ctx.drawImage(this.playerImage,(this.width)/2,(this.height)/2,50,50);
+        this.ctx.drawImage(this.playerImage,this.upLeftGridCornerX + this.gridObjectPadding,
+                                            this.upLeftGridCornerY + this.gridObjectPadding,
+                                            this.gridObjectSize,this.gridObjectSize);
     }
 };
 
