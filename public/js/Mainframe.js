f51f7b691a33d9ca66de20535b76ffbc0b4a581b
Howard N Smith <howard@blacktriangles.com>
2014-03-11 22:08:40 -0500

    cleaning up starting room

--- public/js/Mainframe.js
+++ public/js/Mainframe.js
@@ -50,15 +50,14 @@ Mainframe.prototype.GetLevel = function(height){
         level.addObjectTo(8,1,de);
         de.image = Resources.getImage("stairs");
 
-        level.addObjectTo(1,1,Monster.load('businessman'));
+        //level.addObjectTo(1,1,Monster.load('businessman'));
         //level.addObjectTo(4,4,Pickupable.load('taser'));
         level.addObjectTo(7,7,Pickupable.load('janitors_note'));
         level.addObjectTo(8,8,Pickupable.load('hackable_safe'));
 
-        level.addObjectTo(4,3,Pickupable.load('mirror_shades'));
+        //level.addObjectTo(4,3,Pickupable.load('mirror_shades'));
         
-
-        /*level.addObjectTo(7,7,Pickupable.Items('hackable_save'));*/
+        /*  level.addObjectTo(7,7,Pickupable.Items('hackable_save'));*/
         //spawn all items
         /*for(var p in Pickupable.Items){
             level.addObjectTo(Utilities.randRangeInt(1,8),Utilities.randRangeInt(1,8),Pickupable.load(p));
